60f559a6cc497448d46c801d2f03d2bee9e12098
Tiwann <tiwannbeats@gmail.com>
2025-08-04 15:45:16 +0200

    Moved BufferUtils and ShaderUtils to NovaEngine

--- Example/HelloRenderer2D/Source/BufferUtils.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "Rendering/Vulkan/Buffer.h"
-
-namespace Nova
-{
-    Vulkan::Buffer CreateVertexBuffer(Rendering::Device& device, const void* data, size_t size);
-    Vulkan::Buffer CreateIndexBuffer(Rendering::Device& device, const void* data, size_t size);
-}
--- Example/HelloTexture/Source/ShaderUtils.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "Containers/Array.h"
-#include "Containers/StringView.h"
-
-namespace Nova
-{
-    void CompileShaderToSpirv(StringView filepath, StringView moduleName, Array<uint32_t>& vertSpirv, Array<uint32_t>& fragSpirv);
-    void CompileShaderToSpirv(StringView filepath, StringView moduleName, Array<uint32_t>& computeSpirv);
-}

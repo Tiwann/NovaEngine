59d33fe98a7df32e9950cb9f2423ad5793bee8b6
Tiwann <tiwannbeats@gmail.com>
2025-08-19 18:39:12 +0200

    Work

---
+++ Engine/Source/Editor/Editor.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Runtime/Object.h"
+#include "Containers/StringFormat.h"
+#include "Runtime/Ref.h"
+
+#include <type_traits>
+
+namespace Nova
+{
+    template<class ObjectType> requires std::is_base_of_v<Object, ObjectType>
+    class Editor : public Object
+    {
+    public:
+        explicit Editor(const Ref<ObjectType>& target)
+        : Object(StringFormat("{} Editor", target->GetObjectName())), m_Target(target){}
+
+        ~Editor() override = default;
+
+        virtual void OnInit() {}
+        virtual void OnInspectorGui() {}
+
+    private:
+        Ref<ObjectType> m_Target = nullptr;
+    };
+}

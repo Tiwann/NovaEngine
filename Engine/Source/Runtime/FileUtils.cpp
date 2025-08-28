02befb48e6b08d918da82309d0188e0d86357d55
Tiwann <tiwannbeats@gmail.com>
2025-08-29 01:19:01 +0200

    FileUtils changes

--- Engine/Source/Runtime/FileUtils.cpp
+++ Engine/Source/Runtime/FileUtils.cpp
@@ -1,8 +1,21 @@
 ﻿#include "FileUtils.h"
 #include "Serialization/FileStream.h"
 
-namespace Nova::File
+namespace Nova::FileUtils
 {
+    Array<uint8_t> ReadToBuffer(const StringView filepath)
+    {
+        FileStream stream(filepath, OpenModeFlagBits::ReadBinary);
+        if (!stream.IsOpened()) return {};
+
+        const size_t fileSize = stream.GetSize();
+        Array<uint8_t> buffer(fileSize);
+        const size_t read = stream.Read(buffer.Data(), fileSize);
+        if (read != fileSize) return {};
+
+        return std::move(buffer);
+    }
+
     String ReadToString(const StringView filepath)
     {
         FileStream stream(filepath, OpenModeFlagBits::ReadBinary);
--- Engine/Source/Runtime/FileUtils.h
+++ Engine/Source/Runtime/FileUtils.h
@@ -2,7 +2,8 @@
 #include "Containers/String.h"
 #include "Containers/StringView.h"
 
-namespace Nova::File
+namespace Nova::FileUtils
 {
+    Array<uint8_t> ReadToBuffer(const StringView filepath);
     String ReadToString(const StringView filepath);
 }

2e7e1bc0fe31c3831a97b02a21941f0c2bbca849
Tiwann <tiwannbeats@gmail.com>
2025-12-23 20:47:52 +0100

    Asset pack

--- Programs/AssetPacker/Source/AssetPack.cpp
+++ Programs/AssetPacker/Source/AssetPack.cpp
@@ -1,1 +1,100 @@
 ﻿#include "AssetPack.h"
+#include "Audio/AudioClip.h"
+#include "IO/FileStream.h"
+#include "Runtime/Application.h"
+#include "Runtime/FileUtils.h"
+#include "Runtime/Version.h"
+#include "Utils/TextureUtils.h"
+
+namespace Nova
+{
+    struct AssetPackHeader
+    {
+        uint32_t magic;
+        Version version;
+        uint64_t fileSize;
+        uint64_t assetCount;
+    };
+
+    static void ReadString(Stream& stream, String& string)
+    {
+        uint32_t length = 0;
+        stream.Read(length);
+        string.Resize(length);
+        stream.Read(string, length);
+    }
+
+    bool AssetEntry::Read(Stream& stream)
+    {
+        ReadString(stream, virtualPath);
+        stream.ReadRaw(&uuid, sizeof(UUID));
+        stream.ReadRaw(&assetType, sizeof(AssetType));
+        stream.ReadRaw(&chunkOffset, 2 * sizeof(uint64_t));
+        return true;
+    }
+
+    bool AssetPack::ReadFromFile(StringView filepath)
+    {
+        FileStream stream(filepath, OpenModeFlagBits::ReadBinary);
+        if (!stream.IsOpened()) return false;
+
+        AssetPackHeader header;
+        if (stream.ReadRaw(&header, sizeof(AssetPackHeader)) != sizeof(AssetPackHeader)) return false;
+
+        if (!Memory::Memcmp("NPAK", &header.magic, 4)) return false;
+
+        for (uint32_t i = 0; i < header.assetCount; i++)
+        {
+            AssetEntry entry;
+            entry.Read(stream);
+            m_AssetTable.Add(entry);
+        }
+
+        m_AssetBlob = FileUtils::ReadTillEnd(stream);
+        return true;
+    }
+
+    Ref<Asset> AssetPack::LoadAssetFromVirtualPath(StringView virtualPath)
+    {
+        const auto predicate = [&virtualPath](const AssetEntry& entry) -> bool { return virtualPath == entry.virtualPath; };
+        const AssetEntry* entry = m_AssetTable.Single(predicate);
+        if (!entry) return nullptr;
+        return LoadAssetFromEntry(*entry);
+    }
+
+    Ref<Asset> AssetPack::LoadAssetFromUUID(UUID uuid)
+    {
+        const auto predicate = [&uuid](const AssetEntry& entry) -> bool { return uuid == entry.uuid; };
+        const AssetEntry* entry = m_AssetTable.Single(predicate);
+        if (!entry) return nullptr;
+        return LoadAssetFromEntry(*entry);
+    }
+
+    Ref<Asset> AssetPack::LoadAssetFromEntry(const AssetEntry& entry)
+    {
+        Application& application = Application::GetCurrentApplication();
+        const uint8_t* assetData = &m_AssetBlob[entry.chunkOffset];
+        const uint64_t assetDataSize = entry.chunkSize;
+
+        switch (entry.assetType)
+        {
+        case AssetType::Texture:
+            return LoadTexture(application.GetDevice(), assetData, assetDataSize);
+        case AssetType::AudioClip:
+            {
+                Ref<AudioClip> loadedClip = new AudioClip();
+                if (!loadedClip->LoadFromMemory(assetData, assetDataSize, AudioPlaybackFlagBits::None))
+                    return nullptr;
+                return loadedClip;
+            }
+        case AssetType::Shader:
+            break;
+        case AssetType::StaticMesh:
+            break;
+        default: return nullptr;
+        }
+        return nullptr;
+    }
+
+
+}
--- Programs/AssetPacker/Source/AssetPack.h
+++ Programs/AssetPacker/Source/AssetPack.h
@@ -1,31 +1,57 @@
 ﻿#pragma once
-#include "Runtime/Version.h"
+#include "Containers/StringView.h"
 #include "Containers/String.h"
 #include "Runtime/AssetType.h"
 #include "Runtime/UUID.h"
+#include "Runtime/Ref.h"
 
 #include <cstdint>
 
-
 namespace Nova
 {
+    class Stream;
+    class Asset;
+
+    struct AssetEntry
+    {
+        String virtualPath;
+        UUID uuid;
+        AssetType assetType;
+        uint64_t chunkOffset;
+        uint64_t chunkSize;
+
+        bool Read(Stream& stream);
+    };
+
     class AssetPack
     {
     public:
-        struct AssetEntry
-        {
-            String virtualPath;
-            UUID uuid;
-            AssetType assetType;
-            uint64_t chunkOffset;
-            uint64_t chunkSize;
-        };
+        bool ReadFromFile(StringView filepath);
 
-        struct Header
+        Ref<Asset> LoadAssetFromVirtualPath(StringView virtualPath);
+        Ref<Asset> LoadAssetFromUUID(UUID uuid);
+        Ref<Asset> LoadAssetFromEntry(const AssetEntry& entry);
+
+        template<typename T>
+        Ref<T> LoadAssetFromVirtualPath(StringView virtualPath)
         {
-            uint32_t magic;
-            Version version;
-            uint64_t fileSize;
-        };
+            return LoadAssetFromVirtualPath(virtualPath).As<T>();
+        }
+
+        template<typename T>
+        Ref<T> LoadAssetFromUUID(UUID uuid)
+        {
+            return LoadAssetFromUUID(uuid).As<T>();
+        }
+
+        template<typename T>
+        Ref<T> LoadAssetFromEntry(const AssetEntry& entry)
+        {
+            return LoadAssetFromEntry(entry).As<T>();
+        }
+
+    private:
+        Array<AssetEntry> m_AssetTable;
+        Array<uint8_t> m_AssetBlob;
     };
 }

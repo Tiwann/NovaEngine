ab60f9fa78c1cae7289d858002da0ec5468b91f7
Tiwann <tiwannbeats@gmail.com>
2026-02-24 21:57:13 +0100

    Created DeferredRenderer

---
+++ Engine/Source/Rendering/DeferredRenderer.cpp
@@ -0,0 +1,140 @@
+﻿#include "DeferredRenderer.h"
+#include "Device.h"
+#include "GraphicsPipeline.h"
+#include "RenderPass.h"
+#include "Texture.h"
+
+namespace Nova
+{
+    bool DeferredRenderer::Initialize(const DeferredRendererCreateInfo& createInfo)
+    {
+        if (!createInfo.device) return false;
+        if (createInfo.width == 0 || createInfo.height == 0) return false;
+        if (createInfo.gBufferDescription.numPasses == 0) return false;
+        if (createInfo.gBufferDescription.passesDescriptions == nullptr) return false;
+        if (createInfo.renderTarget == nullptr) return false;
+
+        if (m_IsValid)
+            Invalidate();
+
+        const BufferView<GBufferPassDescription> passesDescriptions{createInfo.gBufferDescription.passesDescriptions, createInfo.gBufferDescription.numPasses};
+
+        m_RenderPass = new RenderPass();
+        for (const GBufferPassDescription& passDesc : passesDescriptions)
+        {
+            RenderPassAttachment attachement;
+            attachement.type = AttachmentType::Color;
+            attachement.loadOp = LoadOperation::Clear;
+            attachement.storeOp = StoreOperation::Store;
+            attachement.clearValue.color = Color::Black;
+            attachement.resolveMode = ResolveMode::Average;
+            m_RenderPass->AddAttachment(attachement);
+        }
+        m_RenderPass->Initialize(0, 0, createInfo.width, createInfo.height);
+
+
+        for (const GBufferPassDescription& passDesc : passesDescriptions)
+        {
+            if (!passDesc.shader)
+                continue;
+
+            GBufferPass pass;
+            pass.description = passDesc;
+
+            const VertexLayout vertexLayout({
+                VertexAttribute{"POSITION", Format::Vector3},
+                VertexAttribute{"TEXCOORDINATE", Format::Vector2},
+                VertexAttribute{"NORMAL", Format::Vector3},
+                VertexAttribute{"TANGENT", Format::Vector3},
+                VertexAttribute{"COLOR", Format::Vector4}
+            });
+
+            const GraphicsPipelineCreateInfo pipelineCreateInfo = GraphicsPipelineCreateInfo()
+                .SetDevice(createInfo.device)
+                .SetShader(passDesc.shader)
+                .SetRenderPass(m_RenderPass)
+                .SetVertexLayout(VertexLayout())
+                .SetMultisampleInfo({1})
+                .SetDepthStencilInfo({
+                    .depthTestEnable = true,
+                    .depthWriteEnable = true,
+                });
+
+            pass.pipeline = createInfo.device->CreateGraphicsPipeline(pipelineCreateInfo);
+            if (!pass.pipeline) return false;
+
+            for (uint32_t i = 0; i < createInfo.device->GetImageCount(); i++)
+            {
+                TextureCreateInfo textureCreateInfo
+                {
+                    .device = createInfo.device,
+                    .usageFlags = TextureUsageFlagBits::Sampled,
+                    .format = pass.description.format,
+                    .width = createInfo.width,
+                    .height = createInfo.height,
+                    .depth = 1,
+                    .mips = 1,
+                    .sampleCount = 1,
+                    .data = nullptr,
+                    .dataSize = 0
+                };
+
+                pass.textures[i] = createInfo.device->CreateTexture(textureCreateInfo);
+                if (!pass.textures[i])
+                {
+                    pass.pipeline->Destroy();
+                    return false;
+                }
+            }
+
+            m_GBufferPasses.Add(pass);
+        }
+
+
+        m_Device = createInfo.device;
+        m_RenderTarget = createInfo.renderTarget;
+        m_Width = createInfo.width;
+        m_Height = createInfo.height;
+        m_GBufferDescription = createInfo.gBufferDescription;
+        m_IsValid = true;
+        return true;
+    }
+
+    void DeferredRenderer::Destroy()
+    {
+        Invalidate();
+    }
+
+    void DeferredRenderer::Resize(const uint32_t width, const uint32_t height)
+    {
+        const DeferredRendererCreateInfo createInfo
+        {
+            .device = m_Device,
+            .renderTarget = m_RenderTarget,
+            .width = width,
+            .height = height,
+            .gBufferDescription = m_GBufferDescription,
+        };
+        Initialize(createInfo);
+    }
+
+    bool DeferredRenderer::IsValid() const
+    {
+        return m_IsValid;
+    }
+
+    void DeferredRenderer::Invalidate()
+    {
+        for (GBufferPass& pass : m_GBufferPasses)
+        {
+            for (uint32_t i = 0; i < m_Device->GetImageCount(); i++)
+                pass.textures[i]->Destroy();
+            pass.pipeline->Destroy();
+        }
+        m_GBufferPasses.Clear();
+        delete m_RenderPass;
+        m_IsValid = false;
+        m_Width = 0;
+        m_Height = 0;
+    }
+}
---
+++ Engine/Source/Rendering/DeferredRenderer.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+#include "Runtime/Format.h"
+#include "Runtime/Object.h"
+#include "Runtime/Ref.h"
+
+namespace Nova
+{
+    class Texture;
+    class Shader;
+    class GraphicsPipeline;
+    class RenderPass;
+
+    enum class GBufferPassType
+    {
+        Albedo,
+        Position,
+        Normal,
+        Tangent,
+        ChannelCombinedCustom,
+        Emission,
+        Lighting,
+    };
+
+    struct GBufferDescription
+    {
+        uint32_t numPasses = 0;
+        struct GBufferPassDescription* passesDescriptions = nullptr;
+    };
+
+    struct GBufferPassDescription
+    {
+        uint32_t passIndex;
+        GBufferPassType type;
+        Format format;
+        Ref<Shader> shader = nullptr;
+    };
+
+    struct GBufferPass
+    {
+        GBufferPassDescription description;
+        Ref<Texture> textures[3] = {nullptr, nullptr, nullptr};
+        Ref<GraphicsPipeline> pipeline;
+    };
+
+    struct DeferredRendererCreateInfo
+    {
+        class Device* device = nullptr;
+        class RenderTarget* renderTarget = nullptr;
+        uint32_t width = 0;
+        uint32_t height = 0;
+        GBufferDescription gBufferDescription;
+    };
+
+    class DeferredRenderer final : public Object
+    {
+    public:
+        DeferredRenderer() = default;
+        ~DeferredRenderer() override = default;
+
+        bool Initialize(const DeferredRendererCreateInfo& createInfo);
+        void Destroy();
+        void Resize(uint32_t width, uint32_t height);
+
+        bool IsValid() const;
+        void Invalidate();
+    private:
+        Device* m_Device = nullptr;
+        RenderPass* m_RenderPass = nullptr;
+        RenderTarget* m_RenderTarget = nullptr;
+        uint32_t m_Width = 0;
+        uint32_t m_Height = 0;
+        bool m_IsValid = false;
+        GBufferDescription m_GBufferDescription;
+        Array<GBufferPass> m_GBufferPasses;
+    };
+}

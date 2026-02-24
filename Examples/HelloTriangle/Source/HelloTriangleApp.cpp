57c8a0ce2f5bbb7674bfb98d9953c34cd2e89bdf
Tiwann <tiwannbeats@gmail.com>
2026-02-24 23:05:30 +0100

    Added HelloTriangleApp

---
+++ Examples/HelloTriangle/Source/HelloTriangleApp.cpp
@@ -0,0 +1,42 @@
+﻿#include "HelloTriangleApp.h"
+using namespace Nova;
+
+extern "C" ::Nova::Application* CreateApplication(int argc, char** argv)
+{
+    return new ::HelloTriangleApp(argc, argv);
+}
+
+Nova::ApplicationConfiguration HelloTriangleApp::GetConfiguration() const
+{
+    ApplicationConfiguration configuration;
+    configuration.applicationName = "HelloTriangle";
+    configuration.vsync = true;
+    configuration.windowFlags = WindowCreateFlagBits::Default;
+    configuration.windowWidth = 800;
+    configuration.windowHeight = 600;
+    return configuration;
+}
+
+void HelloTriangleApp::OnInit()
+{
+
+}
+
+void HelloTriangleApp::OnUpdate(const float deltaTime)
+{
+
+}
+
+void HelloTriangleApp::OnRender(Nova::CommandBuffer& cmdBuffer)
+{
+
+}
+
+Nova::DeviceType HelloTriangleApp::GetRenderDeviceType() const
+{
+#ifdef NOVA_HAS_D3D12
+    return DeviceType::D3D12;
+#else
+    return DeviceType::Vulkan;
+#endif
+}
---
+++ Examples/HelloTriangle/Source/HelloTriangleApp.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Runtime/Application.h"
+
+class HelloTriangleApp final : public Nova::Application
+{
+public:
+    HelloTriangleApp(const int32_t argc, char** argv): Application(argc, argv){}
+
+    Nova::ApplicationConfiguration GetConfiguration() const override;
+    void OnInit() override;
+    void OnUpdate(float deltaTime) override;
+    void OnRender(Nova::CommandBuffer& cmdBuffer) override;
+    Nova::DeviceType GetRenderDeviceType() const override;
+};
